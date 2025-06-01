45724e601bd9e3cf2af7637a25979e1594b47573
maikelreis <maikel_reis@hotmail.com>
2025-05-31 21:23:35 -0300

    Lançar Gastos criado

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ if menu == 'Adicionar Categoria':
     if st.button('Salvar'):
         add_categoria(nome,limite)
         st.success('Categoria adicionada com sucesso')
+
+elif menu == 'Lançar Gastos':
+    categorias = get_categorias()
+    nomes = [c[1] for c in categorias ]
+    selected = st.selectbox('Categorias',nomes)
+    valor = st.number_input('Valor',min_value=0.0)
+    data_gasto = st.date_input("Data",value=date.today())
+
+    if st.button("Salvar Gastos"):
+        cat_id = [c[0] for c in categorias if c[1] == selected]
+        add_despesa(cat_id,valor,data_gasto.strftime("%Y-%m-%d"))
+        st.success("Gasto Registrado")
